48c7c533efc2ea06cb3b5eff3023f65593739f9e
chris260602 <christoperlim20@gmail.com>
2022-03-06 03:39:20 +0700

    Feat: Update Product page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { useEffect, useState } from "react";
-import classes from "./App.module.css"
-
-const App = ()=> {
-  const [ctr,getCtr] = useState(1);
-  useEffect(()=>{
-    document.title = "Travelist"
-},[]);
-  const updateCtr = ()=>{
-    getCtr(ctr + 1);
-  }
-const tes = async ()=>{
-  const response = await fetch("http://localhost:3003/user/getuser/1",{
-    headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-     }
-  })
-  const data = await response.json();
-  console.log(data);
-}
+const App = () => {
   return (
-    <div className={classes.app}>
-      <h1 className="" onClick={tes}>{ctr}</h1>
+    <div>
+      <h1>Hello world</h1>
     </div>
   );
-}
+};
 
 export default App;
